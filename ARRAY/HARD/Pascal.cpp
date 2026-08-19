b5f2d4a28be1779afdfb9b92ad45328b9aef939e
MYNKCHOMIYA <mayankchomia@gmail.com>
2026-08-19 13:04:30 +0530

    "hard one comepleted of array"

---
+++ ARRAY/HARD/Pascal.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>>pascal;
+        for(int i = 0 ; i< numRows; i++){
+            vector<int>row(i+1,1);
+            for(int j =1; j<i;j++){
+                row[j] = pascal[i-1][j-1]+pascal[i-1][j];
+
+            }
+            pascal.push_back(row);
+            
+        }
+        return pascal;
+    }
+};
+
+int main(){
+    Solution sol;
+    sol.generate(5);
+    vector<vector<int>>result = sol.generate(5);
+
+    for(int i =0 ; i<result.size(); i++){
+        for(int j =0;j<result[i].size();j++){
+            cout<<result[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
